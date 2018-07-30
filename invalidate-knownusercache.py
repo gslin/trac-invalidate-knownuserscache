348f3bbf84818f9d86138b66b975d0ea9486fe91
Gea-Suan Lin <gslin@gslin.org>
2018-07-30 14:26:18 +0800

    Invalidate for every authentication request.

--- invalidate-knownusercache.py
+++ invalidate-knownusercache.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python
+
+from trac.core import *
+from trac.web.api import IAuthenticator
+
+class InvalidateKnownUserCacheAuthenticator(Component):
+    implements(IAuthenticator)
+
+    def authenticate(self, req):
+        self.env.invalidate_known_users_cache()
